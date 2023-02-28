0f5b56d1541107035daa085db74bc086b2163473
gretter6 <guillaume.retter5@etu.univ-lorraine.fr>
2023-02-28 12:24:09 +0100

    affichage des plats disponibles (serveur) complété

--- src/main/java/com/example/restaurant/HelloController.java
+++ src/main/java/com/example/restaurant/HelloController.java
@@ -1,20 +1,69 @@
 package com.example.restaurant;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
-import javafx.scene.control.TabPane;
+import javafx.scene.control.*;
 import javafx.scene.layout.AnchorPane;
 
 import java.io.IOException;
+import java.sql.SQLException;
+import java.util.ArrayList;
 
 public class HelloController {
+    private Serveur s = new Serveur();
     @FXML
     private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private TextField numtabreserver;
+
+    @FXML
+    private TextField nbperstabreserver;
+
+    @FXML
+    private DatePicker datereservertabl;
+
+    @FXML
+    private TextField heurereservertabl;
+
+    @FXML
+    private DatePicker datepickerconsultertabl;
+
+    @FXML
+    private TextField heureconsultertabl;
+
+    @FXML
+    private ListView affichagetabl;
+
+    @FXML
+    private TextField numresplat;
+
+    @FXML
+    private TextField numplatcommander;
+
+    @FXML
+    private TextField qtecommander;
+
+    @FXML
+    private ListView affichageplatsdispo;
+
+//    @FXML
+//    private TextField qtecommander;
+//
+//    @FXML
+//    private TextField qtecommander;
+//
+//    @FXML
+//    private TextField qtecommander;
+
+    public HelloController() throws SQLException {
     }
+
+//    @FXML
+//    protected void onHelloButtonClick() {
+//        welcomeText.setText("Welcome to JavaFX Application!");
+//    }
 
     @FXML
     protected void onConnexion(){
@@ -44,8 +93,41 @@ public class HelloController {
     }
 
     @FXML
-    public void onReserverTabl(){
+    public void onReserverTabl() throws SQLException {
+        int numtab = Integer.parseInt(String.valueOf(numtabreserver));
+        int nbpers = Integer.parseInt(String.valueOf(datereservertabl));
+        String date = String.valueOf(datereservertabl);
+        String heure = String.valueOf(heurereservertabl);
+        this.s.reserverTable(numtab,nbpers,date,heure);
+    }
+
+    @FXML
+    public void onConsulterTabl() throws SQLException {
+        String date = String.valueOf(datepickerconsultertabl);
+        String heure = String.valueOf(heureconsultertabl);
+        this.s.consulterTable(date,heure);
+    }
+
+    @FXML
+    public void onCommanderPlat() throws SQLException {
+        int numres = Integer.parseInt(String.valueOf(numresplat));
+        int numplat = Integer.parseInt(String.valueOf(numplatcommander));
+        int qte = Integer.parseInt(String.valueOf(qtecommander));
+        this.s.commanderPlat(numres,numplat,qte);
+    }
+
+    @FXML
+    public void onConsulterPlat() throws SQLException {
+        ArrayList<String> plats;
+        plats = this.s.consulterPlats();
+        ObservableList<String> obs = FXCollections.observableArrayList();
+        obs.addAll(plats);
+
+        this.affichageplatsdispo.setItems(obs);
+
 
     }
+
+
 
 }
